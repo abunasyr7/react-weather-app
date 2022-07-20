f0dd34e5ea528f5fa2236f64f3a960776f184633
abunasyr7 <abunasyr7@yandex.kz>
2022-07-20 10:14:16 +0600

    feat: add .gitinore

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '665822ef3amsh4aec814bb1be007p114552jsne14c91ff7f5f',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "2c409aea0960292b653eec28ef60275d"
